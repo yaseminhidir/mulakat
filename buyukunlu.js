3dd7b574082bd1248659e68db41c0d1636bbe400
yasemin hıdır <yaseminhidir@gmail.com>
2022-05-14 12:00:35 +0300

    büyük harf kontrolü eklendi

--- buyukunlu.js
+++ buyukunlu.js
@@ -1,27 +1,26 @@
 function buyukUnlu(word) {
   let kalinSayisi = 0;
   let inceSayisi = 0;
-  let kalin = ["a", "ı", "o", "u"];
-  let ince = ["e", "i", "ö", "ü"];
+  let kalin = ["A", "I", "O", "U", "a", "ı", "o", "u"];
+  let ince = ["E", "İ", "Ö", "Ü", "e", "i", "ö", "ü"];
   let harfler = word.split("");
 
   for (const harf of harfler) {
     if (kalin.includes(harf)) {
       kalinSayisi += 1;
-      continue;
+      console.log("kalın Sayısı " + kalinSayisi);
     }
     if (ince.includes(harf)) {
       inceSayisi += 1;
-      continue;
+      console.log("ince Sayısı " + inceSayisi);
     }
   }
 
   if (inceSayisi > 0 && kalinSayisi > 0) {
-    alert("büyük ünlü uyumuna uyulmamıştır");
+    return false;
   } else if (inceSayisi > 0 && kalinSayisi == 0) {
-    alert("büyük ünlü uyumuna uyulmuştur");
+    return true;
   } else if (inceSayisi == 0 && kalinSayisi > 0) {
-    alert("büyük ünlü uyumuna uyulmuştur");
+    return true;
   }
 }
-
