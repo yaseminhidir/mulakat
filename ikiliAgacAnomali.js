35396013c303272bb3dd5c65f93d6cc012ea323f
yasemin hıdır <yaseminhidir@gmail.com>
2022-05-17 22:24:13 +0300

    sonsuz döngü revize

--- ikiliAgacAnomali.js
+++ ikiliAgacAnomali.js
@@ -13,7 +13,7 @@ var duzgunAgac = {
     left: {
       left: {
         left: null,
-        righ: null,
+        right: null,
       },
       right: null,
     },
@@ -24,52 +24,36 @@ var duzgunAgac = {
   },
 };
 
-var bozukAgacYaprak = {
-  left: null,
-  right: null,
+var bozukAgacYaprak2 = {
+left: null,
+
+right: null,
 };
+bozukAgacYaprak2.left = bozukAgacYaprak2;
 
-var bozukAgac = {
-  left: {
-    left: {
-      left: bozukAgacYaprak,
-      right: null,
-    },
-    righ: {
-      left: null,
-      right: null,
-    },
-  },
-  right: {
-    left: bozukAgacYaprak,
-    right: {
-      left: null,
-      right: bozukAgacYaprak,
-    },
-  },
-};
-
-function agacKontrol(agac, dahil = []) {
-  if (agac == null) {
-    return true;
-  }
-  if (dahil.includes(agac)) {
-    return false;
-  }
-
-  // recursive
-  var solDogruluk = agacKontrol(agac.left, dahil);
-
-  if (!solDogruluk) {
-    return false;
-  }
-  var sagDogruluk = agacKontrol(agac.right, dahil);
-
-  if (!sagDogruluk) {
-    return false;
-  }
-  dahil.push(agac);
+function agacKontrol(agac, dahil = [], parent = null) {
+if (agac == null) {
   return true;
 }
-console.log("Bozuk Agac Sonuc : ", agacKontrol(bozukAgac)); // false demeli
+if (dahil.includes(agac)) {
+  return false;
+}
+if (agac == parent) {
+  return false;
+}
+// recursive
+var solDogruluk = agacKontrol(agac.left, dahil, agac);
+console.log(agac);
+if (!solDogruluk) {
+  return false;
+}
+var sagDogruluk = agacKontrol(agac.right, dahil, agac);
+
+if (!sagDogruluk) {
+  return false;
+}
+dahil.push(agac);
+return true;
+}
+console.log("Bozuk Agac Sonuc : ", agacKontrol(bozukAgacYaprak2)); // false demeli
 console.log("Duzdun Agac Sonuc : ", agacKontrol(duzgunAgac)); // true demeli
